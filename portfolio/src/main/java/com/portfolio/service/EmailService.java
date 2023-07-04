c9847f1c8f814c35750f93c429938238c91f7256
SUPRANSHU SINGH <ss445198@gmail.com>
2023-07-04 15:08:12 +0530

    nuh uh

--- portfolio/src/main/java/com/portfolio/service/EmailService.java
+++ portfolio/src/main/java/com/portfolio/service/EmailService.java
@@ -10,59 +10,7 @@ import java.util.Properties;
 @Service
 public class EmailService {
 
-    public boolean sendEmail(String subject,String message,String to){
-
-        boolean f=false;
-        String from="supranshus@gmail.com";
-
-
-        //Variable for gmail
-        String host="smtp.gmail.com";
-
-        //get the system properties
-        Properties props=System.getProperties();
-        System.out.println("Properties: "+props);
-
-        //setting important information to properties object
-
-        //host set
-        props.put("mail.smtp.host",host);
-        props.put("mail.smtp.port","465");
-        props.put("mail.smtp.ssl.enable","true");
-        props.put("mail.smtp.auth","true");
-
-        //Step 1: to get the session object
-        Session session=Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("supranshus@gmail.com","bxldquoidjvbcdpb");
-            }
-        });
-
-        session.setDebug(true);
-
-        //Step 2: compose the message [text,multimedia]
-        MimeMessage m=new MimeMessage(session);
-
-        try{
-            m.setFrom(from);
-
-            m.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
-
-            m.setSubject(subject);
-
-            m.setText(message);
-
-            //Step 3:send message using Transport class
-            Transport.send(m);
-
-            System.out.println("Sent successs!!!!");
-            f=true;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return f;
-    }
+    
 
     public boolean sendEmailToPersonalAccount(String subject, String message,String to) {
         boolean f = false;
@@ -104,7 +52,7 @@ public class EmailService {
             // Send the message using Transport class
             Transport.send(m);
 
-            System.out.println("Sent to personal email successfully!");
+            
             f = true;
         } catch (Exception e) {
             throw new RuntimeException(e);
